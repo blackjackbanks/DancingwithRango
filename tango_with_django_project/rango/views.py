7bfdb14ab932d90495015b7dda2ef595fcedb222
A Banks <arliss.banks@gmail.com>
2021-03-12 23:11:01 -0600

    Chapter 7 - Commit - PreExcercies

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
 
 def index(request):
     # List of all categories stored, top 5 desc. Add list to dictionary
     category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     context_dict['categories'] = category_list
+
+    # List of all pages stored, top 5 desc. Add list to dictionary
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict['pages'] = page_list
 
     # Render the response and send it back!
     return render(request, 'rango/index.html', context=context_dict)
@@ -46,5 +52,26 @@ def show_category(request, category_name_slug):
 
     return render(request, 'rango/category.html', context=context_dict)
 
+def add_category(request):
+    form = CategoryForm()
 
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+            # Now that the category is saved, we could confirm this.
+            # For now, just redirect the user back to the index view.
+            return redirect('/rango/')
+        else:
+            # The supplied form contained errors -
+            # just print them to the terminal
+            print(form.errors)
+
+    # Will handle the bad form, new form, or no form supplied case
+    # Render the form with error messages (if any)
+    return render(request, 'rango/add_category.html', {'form':form})
 
