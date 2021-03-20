b660065c6f1f10356094d0d6712bf2f9773bc7d6
A Banks <arliss.banks@gmail.com>
2021-03-20 18:27:11 -0500

    End of Chapter 9 - Commit + Push

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -105,6 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Restricted Page - Login Redirect
+
+LOGIN_URL = 'rango:login'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
